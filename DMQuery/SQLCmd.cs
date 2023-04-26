32442dae928c98b60e6fbc42fc134cce0127369d
Lucas Gabriel <31184517+lgferraz@users.noreply.github.com>
2023-04-26 13:46:33 -0300

    arrumado bug de não rodar query com tabela temp

--- DMQuery/SQLCmd.cs
+++ DMQuery/SQLCmd.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DMQuery
 {
@@ -12,11 +13,35 @@ namespace DMQuery
     {
         public static DataTable Selecionar(string query)
         {
-            SqlCommand cmd = new SqlCommand(query, Conexao.conn);
-            cmd.CommandTimeout=0;
-            DataTable dt = new DataTable();
-            dt.Load(cmd.ExecuteReader());
-            return dt;
+            try
+            {
+                //SqlCommand cmd = new SqlCommand(query, Conexao.conn);
+                //cmd.CommandTimeout = 0;
+                //DataTable dt = new DataTable();
+                //dt.Load(cmd.ExecuteReader());
+                //return dt;
+                using (Conexao.conn)
+                {
+                    Conexao.Conectar();
+                    using (SqlCommand cmd = new SqlCommand(query, Conexao.conn))
+                    {
+                        // criar um DataAdapter para executar a query e preencher um DataTable com o resultado
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+
+                            // fazer algo com o DataTable aqui, por exemplo, exibir os dados em um DataGridView
+                            return dt;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
         }
     }
 }
